e6fa6ac221c5da4cf2e13bb721eea380bac1f55b
Aman <95160406+SybleWilliam@users.noreply.github.com>
2024-06-19 19:27:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(name)
+
+# Define a route for the homepage
+.route('/')
+def hello_world():
+    return 'This bot is made by  and currently it hosted and live for everyone'
+
+# Run the application
+if name == 'main':
+  app.run()
